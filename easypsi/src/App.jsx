f2da3a1b252adeea20909fc6b76470727a8750c1
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2024-09-28 20:01:07 -0300

    page home

--- easypsi/src/App.jsx
+++ easypsi/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Pacientes from './views/Pacientes';
+import Home from './views/Home';
 
 function App() {
 
@@ -8,7 +9,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Pacientes />} />    
+          <Route path="/pacientes" element={<Pacientes />} />   
+          <Route path="/" element={<Home />} />    
         
         </Routes>
       </div>
