f6775d6906082016b756404dcb05a3923bec26ec
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2024-10-01 17:43:59 -0300

    white mode em toda a pag

--- easypsi/src/views/Pacientes.jsx
+++ easypsi/src/views/Pacientes.jsx
@@ -8,12 +8,12 @@ import List from '../components/Lista/List';
 const Pacientes = () => {
   const headers = ['Paciente', 'Horário', 'Próxima consulta', 'Atividade'];
   const [data, setData] = useState([]);
-  const [whiteMode, setWhiteMode] = useState(false); // State to manage white mode
+  const [whiteMode, setWhiteMode] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('URL_DO_SEU_BACKEND');
+        const response = await fetch('URL_DO_SEU_BACKEND'); // Replace with your backend URL
         const jsonData = await response.json();
         
         setData(jsonData.pacientes); 
@@ -25,15 +25,24 @@ const Pacientes = () => {
     fetchData();
   }, []);
 
+  // Toggle the white mode class on the html element
+  useEffect(() => {
+    if (whiteMode) {
+      document.documentElement.classList.add('whitemode');
+    } else {
+      document.documentElement.classList.remove('whitemode');
+    }
+  }, [whiteMode]);
+
   return (
-    <div className={whiteMode ? 'white-mode' : ''}> {/* Apply class based on whiteMode */}
+    <div className={`min-h-screen`}>
       <Sidebar />
       <div className='container-dash'>
         <Titulo showButton={false}>Pacientes</Titulo>
         <Pesquisa showButton={true} />
         <List headers={headers} data={data} />
       </div>
-      <WhiteMode onToggle={setWhiteMode} /> {/* Pass the toggle function to WhiteMode */}
+      <WhiteMode onToggle={setWhiteMode} /> 
     </div>
   );
 };
