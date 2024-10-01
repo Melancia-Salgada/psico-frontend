4a7712353b8771c86ec0e53bf93209e6d4526e22
kauansddsdela <kauanacalmeida17@gmail.com>
2024-10-01 08:27:20 -0300

    Erro sidebar

--- easypsi/src/views/Pacientes.jsx
+++ easypsi/src/views/Pacientes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
-import Sidebar from '../components/Sidebar'
 import Titulo from '../components/Titulo'
 import Lista from '../components/Lista'
 
