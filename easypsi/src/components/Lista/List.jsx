4cdf927b3891e3585781f83d29c4c7d089b2c117
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2024-10-07 21:55:06 -0300

    ajustes finnanceiro e emoji meh

--- easypsi/src/components/Lista/List.jsx
+++ easypsi/src/components/Lista/List.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TemaContexto } from '../WhiteMode';
 
 const List = ({ headers, data }) => {
+
+  const {tema} = useContext(TemaContexto)
+
+  const corErro = tema? "text-red-500" : "text-red-400"
+
   // Verifica se data está vazia ou se é um erro
   const isEmpty = !data || (Array.isArray(data) && data.length === 0);
 
@@ -10,8 +16,17 @@ const List = ({ headers, data }) => {
         {/* Listagem */}
         <div>
           {isEmpty ? (
-            <div className='text-center text-[18px] p-4 text-red-500'>
-              Não possui nenhum cliente cadastrado.
+            <div className={`text-[18px] p-4 ${corErro} transition-colors`}>
+              <span className='justify-center flex mb-10'>
+                Não há nenhum dado cadastrado.
+              </span>
+              <div className='justify-center flex'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-emoji-neutral-fill" viewBox="0 0 16 16">
+                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m-3 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8"/>
+                </svg>
+              
+              </div>
+              
             </div>
           ) : (
             <>
