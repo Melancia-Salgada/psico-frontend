7381a6b1ed3efa43eee43616b5f034f00eac140a
brielalmeida <gabrielalmeida.dev@outlook.com>
2024-10-17 10:54:18 -0300

    login

--- easypsi/src/views/Login.jsx
+++ easypsi/src/views/Login.jsx
@@ -2,16 +2,56 @@ import React, { useContext, useState } from 'react';
 import Logo from '../assets/Logo.png';
 import EyeClosed from '../assets/eye-closed.png';
 import WhiteMode, { TemaContexto } from '../components/WhiteMode';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  {/*Visualizar senha*/}
   const [showPassword, setShowPassword] = useState(false);
-  const {tema} = useContext(TemaContexto)
-  
-
-  const inputBorder = tema ? 'whitemode' : ''; 
-
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+  
+  {/*Darkmode*/}
+  const {tema} = useContext(TemaContexto)
+  const inputBorder = tema ? 'whitemode' : ''; 
+
+  {/*Fofocando com back*/}
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/login', {
+        email,
+        password
+      });
+
+      // Check if the response was successful
+      if (response.status === 200) {
+          
+          // Store token in localStorage
+          //localStorage.setItem('token', response.data); por enquanto vamos armazenar o token no indexedDB
+          const token = response.data;
+          console.log(token)
+          localStorage.setItem('token', token)
+          //await setToken(token);
+          
+          //console.log(response.data)
+          // Successful authentication, redirect to /dashboard page
+          navigate('/home');
+      } else {
+          // Authentication failed, display an error message
+          console.error('Error authenticating user');
+      }
+    } catch (error) {
+      if (error/*error.response && error.response.data && error.response.data.message*/) {
+        console.log("deu erro")
+      } 
+    }
   };
 
   return (
@@ -19,39 +59,50 @@ const Login = () => {
       <div className="mb-20 mt-0">
         <img src={Logo} alt="Logo EasyPSI" />
       </div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-7">
+          <label className="place-self-start">
+            <span className="font-bold">EMAIL</span><br />
+            <input
+              type="email"
+              className={`caixa-texto ${inputBorder}`}
+              placeholder="Digite seu email"
+              id='email'
+              name='email'
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="mb-7 relative">
+          <label className="place-self-start">
+            <span className="font-bold ">SENHA</span><br />
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className={`caixa-texto ${inputBorder}`}
+              placeholder="Digite sua senha"
+              id='senha'
+              name='senha'
+              value={senha}
+              onChange={(e)=>setSenha(e.target.value)}
+            />
+            <img
+              src={showPassword ? EyeOpen : EyeClosed}
+              alt="Mostrar senha"
+              className="absolute right-4 top-14 transform -translate-y-1/2 cursor-pointer w-9 " // Usando classes de largura e altura personalizadas
+              onClick={togglePasswordVisibility}
+            />
+            
 
-      <div className="mb-7">
-        <label className="place-self-start">
-          <span className="font-bold">EMAIL</span><br />
-          <input
-            type="email"
-            className={`caixa-texto ${inputBorder}`}
-            placeholder="DIGITE SEU EMAIL"
-          />
-        </label>
-      </div>
-      <div className="mb-7 relative">
-        <label className="place-self-start">
-          <span className="font-bold ">SENHA</span><br />
-          <input
-            type={showPassword ? 'text' : 'password'}
-            className={`caixa-texto ${inputBorder}`}
-            placeholder="DIGITE SUA SENHA"
-          />
-          <img
-            src={showPassword ? EyeOpen : EyeClosed}
-            alt="Mostrar senha"
-            className="absolute right-4 top-14 transform -translate-y-1/2 cursor-pointer w-9 " // Usando classes de largura e altura personalizadas
-            onClick={togglePasswordVisibility}
-          />
-          
+          </label>
 
-        </label>
+        </div>
+        <button className='btn-entrar' type="submit">ENTRAR</button>
+        
 
-      </div>
-      <a href="/home">
-      <button className='btn-entrar' type="button">ENTRAR</button>
-      </a>
+      </form>
+      
+      
       
 
       <div className='mt-10'>
