253729d1a9b217150304bcba136615781227917a
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2024-10-27 18:48:50 -0300

    adms continuação

--- easypsi/src/views/adm/Gerenciamento.jsx
+++ easypsi/src/views/adm/Gerenciamento.jsx
@@ -3,19 +3,20 @@ import AdminSidebar from './AdminSidebar'
 import Titulo from '../../components/Titulo'
 import WhiteMode from '../../components/WhiteMode'
 import List from '../../components/Lista/List'
+import Pesquisa from '../../components/Lista/Pesquisa';
 
 
 
 const Gerenciamento = () => {
 
-  const headers = ['Email', 'Número', 'CPF'];
+  const headers = ['Email', 'Número', 'CPF', 'Ações'];
 
   // Dados fictícios para simuolar o conteúd
   const [data] = useState([
-    ['a@a.com', '123', '15/11/2024', 'Ativo'],
-    ['Maria Oliveira', '09:30', '18/11/2024', 'Ativo'],
-    ['Carlos Santos', '11:00', 'Não definido', 'Ativo'],
-    ['Ana Paula', '14:00', '25/11/2024', 'Inativo'],
+    ['a@a.com', '123', '101'],
+    ['b@b.com', '456', '456'],
+    ['c@c.com', '789', '789'],
+    ['c@c.com', '101', '123']
   ]); //Mudar para setdata quando integrar o back
 
   return (
@@ -23,6 +24,7 @@ const Gerenciamento = () => {
         <AdminSidebar></AdminSidebar>
         <div className='container-dash'>
             <Titulo showButton={false}>Gerenciamento de admins</Titulo>
+            <Pesquisa></Pesquisa>
             <List headers={headers} data={data}></List>
         </div>
         <WhiteMode />
