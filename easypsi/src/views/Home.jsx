62eb773f482d36e795fddf238e25d7ca9d2636cf
Kauansddsdela <116198015+Kauansddsdela@users.noreply.github.com>
2024-10-27 16:02:35 -0300

    mexi no q a lee fez

--- easypsi/src/views/Home.jsx
+++ easypsi/src/views/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
 
 {/* Tabela de Paciente*/}
-          <div className="bg-cinza shadow-md rounded-lg p-4 w-full md:w-auto flex flex-col h-full">
+          <div className=" rounded-lg p-4 w-full md:w-auto flex flex-col h-full">
             <h2 className="text-xl font-semibold mb-4">Póximas Consultas</h2>
             <div className="overflow-x-auto">
               <table className="table-auto min-w-full md:min-w-[600px]">
@@ -28,7 +28,7 @@ const Home = () => {
                     <th className="px-4 py-2 text-left"><span className='text-white'>Data</span></th>
                   </tr>
                 </thead>
-                <tbody className='bg-branco-whitemode'>
+                <tbody className=''>
                   <tr>
                     <td className="border px-4 py-2">Natália Lee</td>
                     <td className="border px-4 py-2">23/10/2024</td>
@@ -61,7 +61,7 @@ const Home = () => {
           </div>
 
 {/* Segunda Tabela */}
-<div className="bg-cinza shadow-md rounded-lg p-4 w-full md:w-auto flex flex-col h-full">
+<div className="  rounded-lg p-4 w-full md:w-auto flex flex-col h-full">
             <h2 className="text-xl font-semibold mb-4">Financeiro - Pagamento Pendente</h2>
             <div className="overflow-x-auto">
               <table className="table-auto min-w-full md:min-w-[600px]">
@@ -73,7 +73,7 @@ const Home = () => {
                     
                   </tr>
                 </thead>
-                <tbody className='bg-branco-whitemode'>
+                <tbody className=''>
                   <tr>
                   <td className="border px-4 py-2">João Silva</td>
                   <td className="border px-4 py-2">R$180,00</td>
